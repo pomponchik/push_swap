c0e1ca5dc15a04a2b1af2abc36732b7aa3f609ca
Annara farring Halmon paege <ahalmon-@at-f5.21-school.ru>
2019-07-22 23:37:23 +0300

    in progress

--- srcs/fastest_cowboy.c
+++ srcs/fastest_cowboy.c
@@ -11,3 +11,30 @@
 /* ************************************************************************** */
 
 #include <head.h>
+
+
+
+int fastest_cowboy(t_stacks *stacks)
+{
+	size_t temp;
+	size_t temp_2;
+	size_t result;
+	size_t index;
+
+	result = 0;
+	index = 1;
+	temp_2 = how_many_steps_to_get_out(stacks, 0) + \
+	is_sorted_shift_in_mind(stacks->stack_b, stacks->size_b, \
+	(stacks->stack_a)[0]);
+	while (index < size_a)
+	{
+		temp = how_many_steps_to_get_out(stacks, index);
+		temp += is_sorted_shift_in_mind(stacks->stack_b, stacks->size_b, \
+			(stacks->stack_a)[index]);
+		if (temp < temp_2)
+			break ;
+		temp_2 = temp;
+		index++;
+	}
+	return ((stacks->stack_a)[index])
+}
--- srcs/how_many_steps.c
+++ srcs/how_many_steps.c
@@ -24,6 +24,18 @@ size_t how_many_steps_to_get_out(t_stacks *stacks, size_t index)
 	return (down);
 }
 
+size_t how_many_steps_to_down(t_stacks *stacks, size_t index)
+{
+	size_t up;
+	size_t down;
+
+	up = (stacks->size_a - 1) - index;
+	down = index + 1;
+	if (up < down)
+		return (up);
+	return (down);
+}
+
 int do_up(t_stacks *stacks, size_t index)
 {
 	size_t up;
@@ -39,29 +51,4 @@ int do_up(t_stacks *stacks, size_t index)
 int do_down(t_stacks *stacks, size_t index)
 {
 
-}
-
-int fastest_cowboy(t_stacks *stacks)
-{
-	size_t temp;
-	size_t temp_2;
-	size_t result;
-	size_t index;
-
-	result = 0;
-	index = 1;
-	temp_2 = how_many_steps_to_get_out(stacks, 0) + \
-	is_sorted_shift_in_mind(stacks->stack_b, stacks->size_b, \
-	(stacks->stack_a)[0]);
-	while (index < size_a)
-	{
-		temp = how_many_steps_to_get_out(stacks, index);
-		temp += is_sorted_shift_in_mind(stacks->stack_b, stacks->size_b, \
-			(stacks->stack_a)[index]);
-		if (temp < temp_2)
-			break ;
-		temp_2 = temp;
-		index++;
-	}
-	return ((stacks->stack_a)[index])
 }
